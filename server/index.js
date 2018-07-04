ae2649b5c3ac79e32981a1b565fe7ea0429aac05
Adelle Housker <housker@gmail.com>
2018-07-03 20:31:29 -0500

    debugging

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 console.log(__dirname + '/../client')
 
 app.use(express.static(__dirname + '/../client'));
 
-app.listen(3000, () => console.log(`server listening on port ${port}`))
+app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
