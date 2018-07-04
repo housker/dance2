c4273e01a553999066c019ceb8eb7f645b31e716
Adelle Housker <housker@gmail.com>
2018-07-03 20:24:14 -0500

    put index.js inside server folder

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
 
-console.log(__dirname + '/index.html')
+console.log(__dirname + '/../client')
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/../client'));
 
 app.listen(3000, () => console.log(`server listening on port ${port}`))
